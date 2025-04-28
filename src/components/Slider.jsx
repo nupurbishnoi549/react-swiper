60ee96762c5585889de5ebcef5ff039bcf0580e2
Your Name <you@example.com>
2025-04-28 14:52:46 +0530

    swiper done

---
+++ src/components/Slider.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { SIMPLE_STEPS_LIST } from '../utils/app';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const SimpleSteps = () => {
+    return (
+        <div className='max-lg:pb-[44px]'>
+            <div className="2xl:container">
+                <h2 className='text-[#00171F] font-normal text-5xl max-md:text-3xl pb-4 text-center'>
+                    Simple Steps to <span className='font-bold'>Switch</span>
+                </h2>
+                <p className='text-base text-[#4B4B4B] max-w-[610px] mx-auto text-center pb-[58px] max-md:pb-10'>
+                    Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum
+                </p>
+                <Swiper
+                    modules={[Autoplay]}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    className="mySwiper"
+                    breakpoints={{
+                        320: { slidesPerView: 1 },
+                        640: { slidesPerView: 2 },
+                        992: { slidesPerView: 3 },
+                        1200: { slidesPerView: 4 },
+                    }}
+                >
+                    {SIMPLE_STEPS_LIST.map((obj, i) => (
+                        <SwiperSlide key={i}>
+                            <div className='relative group overflow-hidden max-md:max-h-[450px] max-w-[360px] w-full mx-auto'>
+                                <img className='max-w-[360px] w-full' src={obj.stepImg} alt={obj.alt} />
+                                <div className='absolute group-hover:h-0 group-hover:w-0 group-hover:top-1/2 group-hover:left-full group-hover:translate-y-[70%] group-hover:translate-x-[-50%] group-hover:opacity-80 group-hover:z-10 duration-700 ease-in-out bg-[#EAF9FFF0] opacity-94 top-0 left-0 w-full h-full'>
+                                    <div className='relative w-full h-full'>
+                                        <p className={`absolute translate-x-[-50%] text-center text-[#00171F] -rotate-90 min-w-[574px] group-hover:hidden ${i === 0 ? "top-1/2 left-1/2" : i === 1 ? "top-[68%] left-1/2" : i === 2 ? "top-[74%] left-1/2" : i === 3 ? "top-[65.5%] left-1/2" : ""}`}>{obj.imgContent}</p>
+                                    </div>
+                                </div>
+                                <div className='bg-[#003459] absolute top-[-50%] group-hover:top-[73%] group-hover:bottom-0 duration-700 ease-in-out w-full p-8'>
+                                    <h4 className='text-[32px] font-normal pb-3 text-white'>{obj.heading}</h4>
+                                    <p className='text-white text-base'>{obj.description}</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div>
+    );
+};
+
+export default SimpleSteps;
---
+++ src/utils/app.js
@@ -0,0 +1,35 @@
+import Card1 from '../assets/images/png/Card1.png';
+import Card2 from '../assets/images/png/Card2.png';
+import Card3 from '../assets/images/png/Card3.png';
+import Card4 from '../assets/images/png/Card4.png';
+
+export const SIMPLE_STEPS_LIST = [
+    {
+        stepImg: Card1,
+        alt: 'step-location',
+        imgContent: '1. Enter Your Location and Preferences',
+        heading: '1. Your Location',
+        description: 'Enter your location and preferences to find the best energy, gas, and internet plans for you.'
+    },
+    {
+        stepImg: Card2,
+        alt: 'step-compare',
+        imgContent: '2. Compare Providers',
+        heading: '2. Compare',
+        description: 'Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum'
+    },
+    {
+        stepImg: Card3,
+        alt: 'step-switch-easily',
+        imgContent: '3. Switch Easily',
+        heading: '3. Switch Easily',
+        description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.'
+    },
+    {
+        stepImg: Card4,
+        alt: 'step-best-plan',
+        imgContent: '4. Choose the Best Plan',
+        heading: '4. Best Plan',
+        description: 'Enter your location and preferences to find the best energy, gas, and internet plans for you.'
+    },
+];
