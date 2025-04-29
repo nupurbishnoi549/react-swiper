d7a5cd9ac349798ccbc9e8e33a3e552e7deb0ebd
Your Name <you@example.com>
2025-04-29 12:37:00 +0530

    done

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,12 +9,6 @@ const SimpleSteps = () => {
     return (
         <div className='max-lg:pb-[44px] py-11'>
             <div className="2xl:container max-sm:px-3">
-                <h2 className='text-[#00171F] font-normal text-5xl max-md:text-3xl pb-4 text-center'>
-                    Simple Steps to <span className='font-bold'>Switch</span>
-                </h2>
-                <p className='text-base text-[#4B4B4B] max-w-[610px] mx-auto text-center pb-[58px] max-md:pb-10'>
-                    Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum
-                </p>
                 <Swiper
                     modules={[Autoplay]}
                     autoplay={{
